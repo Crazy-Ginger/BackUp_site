a2adc8bc4c1b279ad668244d57d00679a55beb4a
Rebecca Wardle <mattydwardle@gmail.com>
2020-03-22 04:00:23 +0000

    Added more to todo list and added a restricted page for testing

--- script.js
+++ script.js
@@ -7,3 +7,5 @@ function clearText(id)
 {
     document.getElementById(id).value = "";
 }
+
+//implement an algorithm that hashes the users files and then sends that file to the server to check against the server's own hashes
